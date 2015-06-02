00f02561e1c252a839530a8449c813b8718e624d
Pablo Rosales <me@mooddha.com>
2015-06-01 20:53:40 -0600

    Adding package and Gruntfile base files

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        package: grunt.file.readJSON('package.json'),
+        concat: {
+            'dist': {
+                src: ['src/jquery.elevator.js'],
+                dest: 'dist/jquery.elevator.js'
+            }
+        },
+        uglify: {
+            'dist': {
+                src: 'dist/jquery.elevator.js',
+                dest: 'dist/jquery.elevator.min.js'
+            }
+        },
+        jshint: {
+            options: { jshintrc: true },
+            code: { src: ['src/*.js'] },
+            tests: { src: ['tests/*.js'] }
+        },
+        watch: {
+            js: {
+                files: [
+                    'src/*.js',
+                    'tests/*.js'
+                ],
+                tasks: [
+                    'compile',
+                    'minify'
+                ]
+            },
+            css: {
+                files: [
+                    'src/*.css'
+                ],
+                tasks: [
+                    'compile',
+                    'minify'
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['compile', 'minify']);
+    grunt.registerTask('compile', ['concat:dist']);
+    grunt.registerTask('minify', ['uglify']);
+
+};
