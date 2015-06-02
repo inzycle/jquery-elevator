e86f262b192f14fd192d441a917448b140c2a585
Pablo Rosales <me@mooddha.com>
2015-06-02 12:00:19 -0600

    Cleaning up code with better implementation.

--- src/jquery.elevator.js
+++ src/jquery.elevator.js
@@ -1,198 +1,157 @@
-(function($) {
- 
-    $.elevator = function(sections, options) {
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(function($) {
 
-        var defaults = {
-                align_left: false,
-                align_top: false,
-                top: true,
-                bottom: true,
-                nav: false,
-                margin: 100
-            },
-            asClass = function (c) { return c.replace('.', ''); },
-            asId = function (c) { return c.replace('#', ''); },
-            settings = $.extend({}, defaults, options),
-            selectors = {
-                elevator: '.elevator',
-                top: '.jelevator-document-anchor-top',
-                bottom: '.jelevator-document-anchor-bottom',
-                section: '.jelevator-section-list',
-                anchor: '.jelevator-section-anchor',
-                anchor_top: '#jelevator-top',
-                anchor_bottom: '#jelevator-bottom'
-            };
+    var defaults = {
+            align: 'bottom right',
+            show_top: true,
+            show_bottom: true,
+            navigation: [],
+            margin: 100
+        },
+        settings = {},
+        $doc = $(document),
+        $win = $(window),
+        $div = $('<div>'),
+        top_link,
+        bottom_link;
 
-        function createElement(elem, classes) {
-            return $(elem).addClass(asClass(classes));
+    $.elevator = function(options) {
+
+        settings = $.extend({}, defaults, options);
+
+        function scrollTo(target) {
+            $('html, body').animate({
+                scrollTop: target
+            }, 1000);
         }
 
-        var align_horizontal = settings.align_left == true ? 'align-left' : 'align-right';
-        var align_vertical = settings.align_top == true ? 'align-top' : 'align-bottom';
-        var dom = $('<div>').addClass('jelevator ' + align_horizontal + ' ' + align_vertical);
-        
-        /**
-         * Create the top anchor
-         */
-        function createTopAnchor() {
-            return createElement('<div>', selectors.elevator).attr('id', asId(selectors.anchor_top));
+        function getDocHeight() {
+            return $doc.height();
         }
-        
-        /**
-         * Create the go to top link
-         */
-        function createTopLink() {
-            return createElement('<a>', selectors.top).attr('href', selectors.anchor_top).html('&#9650;');
-        }
-        
-        /**
-         * Create the bottom anchor
-         */
-        function createBottomAnchor() {
-            return createElement('<div>', selectors.elevator).attr('id', asId(selectors.anchor_bottom));
-        }
-        
-        /**
-         * Create the go to bottom link
-         */
-        function createBottomLink() {
-            return createElement('<a>', selectors.bottom).attr('href', selectors.anchor_bottom).html('&#9660;');
-        }
-        
-        /**
-         * Create the navigation for sections
-         */
-        function createNav() {
-            return createElement('<nav>', selectors.section);
-        }
-        
-        /**
-         * Create a single link for sections
-         */
-        function createSectionLink(name, title) {
-            return createElement('<a>', selectors.anchor).attr('href', name).text(title);
-        }
-        
-        /**
-         * Create the links for sections
-         */
-        function createSectionsLinks() {
-            var links = [];
-            sections.forEach(function(element) {
-                links.append(createSectionLink(element.name, element.title));
+
+        function bottomLink() {
+
+            var _class = 'jq-elevator-bottom';
+
+            bottom_link = $('<a>')
+                .addClass(_class)
+                .attr('href', '#' + _class)
+                .html('&#9660;');
+
+            bottom_link.on('click.' + _class, function(e) {
+                scrollTo(getDocHeight());
+                e.preventDefault();
             });
+
+            $div.append(bottom_link);
+
         }
-    
-        /**
-         * Create all the needed elements and append to body
-         */
-        function createUI() {
-            
-            var bottom, 
-                nav,
-                nav_list,
-                top,
-                $body = $('body');
 
-            if (settings.bottom) {
-                $body.append(createBottomAnchor());
-                bottom = createBottomLink();
-                dom.append(bottom);
-            }
+        function topLink() {
 
-            if (settings.nav) {
-                nav = createNav();
-                nav_list = createSectionsLinks();
-                nav.append(nav_list);
-                dom.append(nav);
-            }
+            var _class = 'jq-elevator-top';
 
-            if (settings.top) {
-                $body.prepend(createTopAnchor());
-                top = createTopLink();
-                dom.append(top);
-            }
-            
-            $body.append(dom);
-            
-        }
-        
-        /**
-         * Add click event to button
-         */
-        function addScrollClickEvent(elem, target) {
-            // TODO: Check AJAX implementation support
-            $(elem).click(function() {
-                $("html, body").animate({
-                    scrollTop: target
-                }, 1000);
-                return false;
+            top_link = $('<a>')
+                .addClass(_class)
+                .attr('href', '#') // # points to the top of the page
+                .html('&#9650;');
+
+            top_link.on('click.' + _class, function(e) {
+                scrollTo(0);
+                e.preventDefault();
             });
+
+            $div.append(top_link);
+
         }
-        
-        /**
-         * Add scroll position detection event
-         */
-        function addScrollDetectEvent() {
-            var $win = $(window);
-            var $doc = $(document);
-            var height = $doc.height();
-            var margin = settings.margin;
+
+        function navigationLinks() {
+
+        }
+
+        function align() {
+
+            var positions = settings.align.split(' ');
+
+            if (positions.indexOf('top') >= 0) {
+                $div.addClass('align-top');
+            }
+
+            if (positions.indexOf('bottom') >= 0) {
+                $div.addClass('align-bottom');
+            }
+
+            if (positions.indexOf('left') >= 0) {
+                $div.addClass('align-left');
+            }
+
+            if (positions.indexOf('right') >= 0) {
+                $div.addClass('align-right');
+            }
+
+        }
+
+        function atTop() {
+            return $doc.scrollTop() <= settings.margin;
+        }
+
+        function atBottom() {
+            return $win.scrollTop() + $win.height() > getDocHeight() - settings.margin;
+        }
+
+        function init() {
+
+            $div.addClass('jq-elevator');
+
+            align();
+
+            if (settings.show_top) {
+                topLink();
+            }
+
+            if (settings.navigation.length > 0) {
+                navigationLinks();
+            }
+
+            if (settings.show_bottom) {
+                bottomLink();
+            }
+
+            if (atBottom()) {
+                bottom_link.addClass('item');
+            }
+            else if (atTop()) {
+                top_link.addClass('item');
+            }
+
             $doc.scroll(function() {
-                var pos = $doc.scrollTop();
-                var at_top = pos <= margin;
-                var at_bottom = $win.scrollTop() + $win.height() > height - margin;
-                if (at_top) {
-                    $(selectors.top).addClass('item');
-                    $(selectors.bottom).removeClass('item');
+                if (atTop()) {
+                    top_link.addClass('item');
+                    bottom_link.removeClass('item');
                 }
-                else if (at_bottom) {
-                    $(selectors.top).removeClass('item');
-                    $(selectors.bottom).addClass('item');
+                else if (atBottom()) {
+                    bottom_link.addClass('item');
+                    top_link.removeClass('item');
+                }
+                else {
+                    top_link.removeClass('item');
+                    bottom_link.removeClass('item');
                 }
             });
+
+            $('body').append($div);
+
         }
-        
-        /**
-         * Add the needed events
-         */
-        function addEvents() {
-            addScrollClickEvent(selectors.bottom, $(document).height());
-            addScrollClickEvent(selectors.top, 0);
-            addScrollDetectEvent();
-        }
-        
-        // Init
-        
-        createUI();
-        addEvents();
- 
-    };
-    
-}(jQuery));
 
-/* -- HTML Prototype
-----> ??
-<div class="jelevator align-right align-bottom">
-    <!-- class="align-left" -->
-    <!-- class="align-right" -->
-    <!-- class="align-top" -->
-    <!-- class="align-bottom" -->
+        init();
 
-    <a href="#Document.BottomAnchor" class="jelevator-document-anchor-bottom">&#9660;</a>
-        <!-- class="list-element" -->
+    }
 
-    <!--
-    <nav class="jelevator-section-list">
-    
-        <a href="#Section.anchor" class="jelevator-section-anchor">Section.Title</a>
-        
-    </nav>
-    -->
-
-        <!-- class="list-element" -->
-    <a href="#Document.TopAnchor" class="jelevator-document-anchor-top">&#9650;</a>
-
-</div>
-----> ??
--- */
+}));
