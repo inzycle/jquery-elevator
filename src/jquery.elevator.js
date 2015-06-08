fb18f0ddd0175c424bd123a1b96a8abe7049ee33
Sigma <sm.salvador@gmail.com>
2015-06-08 03:38:49 -0500

    Updated jQuery-Elevator - Updated CSS ( new classes ) - Updated functions names - Updated margin functionality - Added top/bottom item functionality - Added navigation links functionality - Added autoID generator functionality - Updated options control - Updated classes control - Added shape functionality

--- src/jquery.elevator.js
+++ src/jquery.elevator.js
@@ -11,9 +11,12 @@
     var defaults = {
             align: 'bottom right',
             show_top: true,
+            item_top: false,
             show_bottom: true,
+            item_bottom: false,
             navigation: [],
-            margin: 100
+            margin: 100,
+            speed: 1000
         },
         settings = {},
         $doc = $(document),
@@ -29,42 +32,36 @@
         function scrollTo(target) {
             $('html, body').animate({
                 scrollTop: target
-            }, 1000);
+            }, settings.speed);
         }
 
         function getDocHeight() {
             return $doc.height();
         }
 
-        function bottomLink() {
+        function createTopLink() {
 
-            var _class = 'jq-elevator-bottom';
+            var _class = 'jq-top jq-mid',
+                _anchor = '#',
+                _item_top = settings.item_top;
 
-            bottom_link = $('<a>')
-                .addClass(_class)
-                .attr('href', '#' + _class)
-                .html('&#9660;');
-
-            bottom_link.on('click.' + _class, function(e) {
-                scrollTo(getDocHeight());
-                e.preventDefault();
-            });
-
-            $div.append(bottom_link);
-
-        }
-
-        function topLink() {
-
-            var _class = 'jq-elevator-top';
+            if(_item_top && typeof(_item_top) == 'object'){
+                _item_top.attr('id') ? _item_top.attr('id') : _item_top.attr('id','jq-TOP');
+                _anchor = '#' + _item_top.attr('id');
+            }
 
             top_link = $('<a>')
                 .addClass(_class)
-                .attr('href', '#') // # points to the top of the page
+                .attr('href', _anchor)
                 .html('&#9650;');
 
             top_link.on('click.' + _class, function(e) {
-                scrollTo(0);
+                if(_item_top && typeof(_item_top) == 'object') {
+                    scrollTo(_item_top.offset().top);
+                }
+                else {
+                    scrollTo(0);
+                }
                 e.preventDefault();
             });
 
@@ -72,11 +69,98 @@
 
         }
 
-        function navigationLinks() {
+        function createNavigationLinks() {
+
+            var _class = 'jq-item jq-sml',
+                _anchor = '#',
+                _navigation = settings.navigation;
+
+            $.each(_navigation, function(key, val){
+
+                $(val).attr('id') ? $(val).attr('id') : $(val).attr('id','jq-' + parseInt($(val).offset().top));
+                _anchor = '#' + $(val).attr('id');
+
+                var item_link = $('<a>')
+                    .addClass(_class)
+                    .attr('href', _anchor)
+                    .html('&#9664;');
+
+                $div.append(item_link);
+
+            });
+
+            // TODO: Update jq-item handler to namespaces
+
+            $(document).on('click', '.jq-item', function(e) {
+                var _item = $($(this).attr('href'));
+                scrollTo(_item.offset().top);
+                e.preventDefault();
+            });
 
         }
 
-        function align() {
+        function createBottomLink() {
+
+            var _class = 'jq-bottom jq-mid',
+                _anchor = '#',
+                _item_bottom = settings.item_bottom;
+
+            if(_item_bottom && typeof(_item_bottom) == 'object'){
+                _item_bottom.attr('id') ? _item_bottom.attr('id') : _item_bottom.attr('id','jq-BOTTOM');
+                _anchor = '#' + _item_bottom.attr('id');
+            }
+
+            bottom_link = $('<a>')
+                .addClass(_class)
+                .attr('href', _anchor)
+                .html('&#9660;');
+
+            bottom_link.on('click.' + _class, function(e) {
+                if(_item_bottom && typeof(_item_bottom) == 'object') {
+                    scrollTo(_item_bottom.offset().top + (_item_bottom.outerHeight(true) - $win.height()));
+                }
+                else {
+                    scrollTo(getDocHeight());
+                }
+                e.preventDefault();
+            });
+
+            $div.append(bottom_link);
+
+        }
+
+        function atTop() {
+
+            var _item_top = settings.item_top,
+                _ret = null;
+
+            if(_item_top && typeof(_item_top) == 'object'){
+                _ret = $win.scrollTop() <= _item_top.offset().top + settings.margin;
+            }
+            else {
+                _ret = $win.scrollTop() <= settings.margin;
+            }
+
+            return _ret;
+        }
+
+        function atBottom() {
+
+            var _item_bottom = settings.item_bottom,
+                _ret = null;
+
+            if(_item_bottom && typeof(_item_bottom) == 'object'){
+                _ret = $win.scrollTop() >= _item_bottom.offset().top - $win.height() - settings.margin;
+            }
+            else {
+                _ret = $win.scrollTop() + $win.height() >= getDocHeight() - settings.margin;
+            }
+
+            return _ret;
+
+        }
+
+        function setAlign() {
 
             var positions = settings.align.split(' ');
 
@@ -98,51 +182,67 @@
 
         }
 
-        function atTop() {
-            return $doc.scrollTop() <= settings.margin;
-        }
+        function setShape() {
 
-        function atBottom() {
-            return $win.scrollTop() + $win.height() > getDocHeight() - settings.margin;
+            var shape = settings.shape;
+
+            switch (shape) {
+                case 'square':
+                    $div.addClass('square');
+                    break;
+                case 'rounded':
+                    $div.addClass('rounded');
+                    break;
+                case 'circle':
+                    $div.addClass('circle');
+                    break;
+                default:
+                    $div.addClass('circle');
+            }
+
         }
 
         function init() {
 
             $div.addClass('jq-elevator');
 
-            align();
+            setAlign();
+            setShape();
 
             if (settings.show_top) {
-                topLink();
+                createTopLink();
             }
 
             if (settings.navigation.length > 0) {
-                navigationLinks();
+                createNavigationLinks();
             }
 
             if (settings.show_bottom) {
-                bottomLink();
+                createBottomLink();
             }
 
-            if (atBottom()) {
-                bottom_link.addClass('item');
+            if (atTop()) {
+                settings.show_top ? top_link.removeClass('jq-mid').addClass('jq-sml') : false;
+                settings.show_bottom ? bottom_link.removeClass('jq-mid').addClass('jq-big') : false;
             }
-            else if (atTop()) {
-                top_link.addClass('item');
+            else if (atBottom()) {
+                settings.show_top ? top_link.removeClass('jq-mid').addClass('jq-big') : false;
+                settings.show_bottom ? bottom_link.removeClass('jq-mid').addClass('jq-sml') : false;
             }
 
             $doc.scroll(function() {
+
                 if (atTop()) {
-                    top_link.addClass('item');
-                    bottom_link.removeClass('item');
+                    settings.show_top ? top_link.removeClass('jq-mid').addClass('jq-sml') : false;
+                    settings.show_bottom ? bottom_link.removeClass('jq-mid').addClass('jq-big') : false;
                 }
                 else if (atBottom()) {
-                    bottom_link.addClass('item');
-                    top_link.removeClass('item');
+                    settings.show_top ? top_link.removeClass('jq-mid').addClass('jq-big') : false;
+                    settings.show_bottom ? bottom_link.removeClass('jq-mid').addClass('jq-sml') : false;
                 }
                 else {
-                    top_link.removeClass('item');
-                    bottom_link.removeClass('item');
+                    settings.show_top ? top_link.removeClass('jq-big jq-sml').addClass('jq-mid') : false;
+                    settings.show_bottom ? bottom_link.removeClass('jq-big jq-sml').addClass('jq-mid') : false;
                 }
             });
 
